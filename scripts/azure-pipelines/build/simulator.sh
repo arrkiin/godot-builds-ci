b1ae04ecb7977488116e9f9088fbcaffc9b9a455
arrkiin <arrkiin@gmail.com>
2019-07-07 22:58:33 +0200

    Added alle matrix positions after successful test

--- scripts/azure-pipelines/build/simulator.sh
+++ scripts/azure-pipelines/build/simulator.sh
@@ -6,13 +6,17 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-scons platform=iphone arch=x86_64 tools=no target=release_debug \
-      "${SCONS_FLAGS[@]}" "${SCONS_TMPL_FLAGS[@]}"
+for target in "release_debug" "release"; do
+  scons platform=iphone arch=x86_64 tools=no target=$target \
+        "${SCONS_FLAGS[@]}" "${SCONS_TMPL_FLAGS[@]}"
+done
 
 # Create export templates ZIP archive
-mv "bin/libgodot.iphone.opt.debug.x86_64.a" "libgodot.iphone.opt.debug.simulator.a"
+mv "bin/libgodot.iphone.opt.debug.x86_64.a" "libgodot.iphone.debug.fat.a"
+mv "bin/libgodot.iphone.opt.x86_64.a" "libgodot.iphone.release.fat.a"
 touch "data.pck"
 zip -r9 \
     "$BUILD_ARTIFACTSTAGINGDIRECTORY/templates/simulator.zip" \
-    "libgodot.iphone.opt.debug.simulator.a" \
+    "libgodot.iphone.debug.fat.a" \
+    "libgodot.iphone.release.fat.a" \
     "data.pck"
