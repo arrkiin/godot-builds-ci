5d6a817f92143999023ece47ca1e455db4711d33
arrkiin <arrkiin@gmail.com>
2019-07-10 06:30:33 +0200

    Divide building for android in seperate build-target steps

--- scripts/azure-pipelines/build/android.sh
+++ scripts/azure-pipelines/build/android.sh
@@ -7,19 +7,22 @@ set -euo pipefail
 IFS=$'\n\t'
 
 export ANDROID_NDK_ROOT=$ANDROID_HOME/ndk-bundle
+export GODOT_DIR=$PWD
+
+if [[ "$BUILD_TARGET" == "debug" ]]; then
+  scons_target="release_debug"
+else
+  scons_target="release"
+fi
 
 # Build Android export template
-for target in "release_debug" "release"; do
-  for arch in "armv7" "arm64v8" "x86"; do
-  scons platform=android tools=no target="$target" android_arch="$arch" \
+for arch in "armv7" "arm64v8" "x86"; do
+    scons platform=android tools=no target="$scons_target" android_arch="$arch" \
         "${SCONS_FLAGS[@]}" "${SCONS_TMPL_FLAGS[@]}"
-  done
 done
 
 # Create an APK and move it to the artifacts directory
 cd "$GODOT_DIR/platform/android/java/"
 ./gradlew build
-for target in "debug" "release"; do
-  mv "$GODOT_DIR/bin/android_$target.apk" \
-      "$BUILD_ARTIFACTSTAGINGDIRECTORY/templates/"
-done
+mv "$GODOT_DIR/bin/android_$BUILD_TARGET.apk" \
+    "$BUILD_ARTIFACTSTAGINGDIRECTORY/templates/"
